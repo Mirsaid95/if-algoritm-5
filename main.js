a1b3cd080f9a67a88898d1363b86563f20e5ce55
Mirsaid <akbaraliyev522@mail.ru>
2024-08-07 13:27:27 +0500

    main

--- main.js
+++ main.js
@@ -1,5 +1,110 @@
 // Home Task-1
 
-let a = prompt(" Uch xonali son kiriting")
+// let a = 123;
 
-if(a > 0 &&)
+
+// if( a > 99 && a < 1000){
+//     if(a % 2 != 0){
+    // console.log(true);
+// }
+// }else{
+//     console.log(false);
+// }
+
+// Home Task-2
+
+// let a = prompt("Birinchi sonni kiriting")
+// let b = prompt("Birinchi sonni kiriting")
+// let c = prompt("Birinchi sonni kiriting")
+
+// if( a == b || a == c || b == c){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// Home Task-3
+
+// let a = prompt("birinchi sonni kiriting")
+// let b = prompt("birinchi sonni kiriting")
+// let c = prompt("birinchi sonni kiriting")
+
+// if( a == -b || a == -c || b == -c){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// Home Task-4
+
+// let a = promt("Uch xonali son kiriting");
+
+// b = a % 10;
+// c = (a - b) % 100 / 10;
+// d = parseInt(a / 100)
+
+// if(a > 99 && a < 1000){
+//     if( b != c && b != d && c != d){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+// }else{
+//     console.log(false);
+// }
+
+// Home Task-5
+
+// let a = 234;
+
+// birlar = a % 10; // birlar = 3;
+// onlar = (a - birlar) % 100 / 10; // onlar = 2;
+// yuzlar = parseInt(a / 100);
+
+// if( a > 99 && a < 1000){
+//     if ( yuzlar < onlar && onlar < birlar){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+// }else{
+//     console.log(false);
+// }
+
+
+// Home Task-6 
+
+// let a = 321;
+
+// birlar = a % 10; // birlar = 3;
+// onlar = (a - birlar) % 100 / 10; // onlar = 2;
+// yuzlar = parseInt(a / 100);
+
+// if( a > 99 && a < 1000){
+//     if ( yuzlar < onlar && onlar < birlar || yuzlar > onlar && onlar > birlar){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+// }else{
+//     console.log(false);
+// }
+
+// Home Task-7
+
+// let a = 242;
+
+// birlar = a % 10; // birlar = 3;
+// onlar = (a - birlar) % 100 / 10; // onlar = 2;
+// yuzlar = parseInt(a / 100);
+
+// if( a > 99 && a < 1000){
+//     if (yuzlar == birlar){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+// }else{
+//     console.log(false);
+// }
+
